56e13dfdb4a7c122905755a2b57685092d553d97
Maximilian Rutyna <mrutyna777@gmail.com>
2016-06-28 15:50:01 -0700

    Add Sessions and Users Controllers and respective routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: {format: :json} do
+    resource :users, only: [:create]
+    resource :session, only: [:create, :destroy, :show]
+  end
+
+  root "static_pages#root"
+end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +62,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
