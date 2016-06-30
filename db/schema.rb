f83e1e0175cb57dddd8da49ed213f633f1e89742
Maximilian Rutyna <mrutyna777@gmail.com>
2016-06-30 16:26:44 -0700

    Manually Edited Scheme file instead of creating migrations- generated entire DB

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,50 @@ ActiveRecord::Schema.define(version: 20160628223352) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+create_table "comments", force: :cascade do |t|
+  t.text     "body",       null: false
+  t.integer  "x_pos",      null: false
+  t.integer  "y_pos",      null: false
+  t.integer  "user_id",    null: false
+  t.integer  "photo_id",   null: false
+  t.datetime "created_at", null: false
+  t.datetime "updated_at", null: false
+end
+
+add_index "comments", ["photo_id"], name: "index_comments_on_photo_id", using: :btree
+add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+create_table "photos", force: :cascade do |t|
+  t.string   "title",       null: false
+  t.text     "description", null: false
+  t.string   "photo_url",   null: false
+  t.integer  "user_id",     null: false
+  t.integer  "album_id"
+  t.datetime "created_at",  null: false
+  t.datetime "updated_at",  null: false
+end
+
+add_index "photos", ["album_id"], name: "index_photos_on_album_id", using: :btree
+add_index "photos", ["user_id"], name: "index_photos_on_user_id", using: :btree
+
+create_table "taggings", force: :cascade do |t|
+  t.integer  "tag_id",     null: false
+  t.integer  "photo_id",   null: false
+  t.datetime "created_at", null: false
+  t.datetime "updated_at", null: false
+end
+
+add_index "taggings", ["photo_id"], name: "index_taggings_on_photo_id", using: :btree
+add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+
+create_table "tags", force: :cascade do |t|
+  t.string   "name",       null: false
+  t.datetime "created_at", null: false
+  t.datetime "updated_at", null: false
+end
+
+add_index "tags", ["name"], name: "index_tags_on_name", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
     t.string   "password_digest", null: false
@@ -24,6 +68,7 @@ ActiveRecord::Schema.define(version: 20160628223352) do
     t.datetime "updated_at",      null: false
   end
 
+  add_index "users", ["session_token"], name: "index_users_on_session_token", using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
