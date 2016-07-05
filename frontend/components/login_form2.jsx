afed4f9946a6cb32c0d13ff9bf9458805631b4dd
Maximilian Rutyna <mrutyna777@gmail.com>
2016-07-05 14:54:17 -0700

    Fixed Home Page Redirecting to Photos upon Login

--- frontend/components/login_form2.jsx
+++ frontend/components/login_form2.jsx
@@ -87,7 +87,7 @@ const LoginForm = React.createClass({
 
   redirectIfLoggedIn() {
     if (SessionStore.isUserLoggedIn()) {
-      this.context.router.push("/");
+      this.context.router.push("/photos");
     }
   },
 
