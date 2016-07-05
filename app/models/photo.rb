51c5a211f202861025a3955986e5b747b6ea8cd5
Maximilian Rutyna <mrutyna777@gmail.com>
2016-07-04 21:20:08 -0700

    Fixed Photo and Tags Relationship

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,14 @@
 class Photo < ActiveRecord::Base
   validates :title, :description, :photo_url, :user_id, presence: true
 
+
   has_many :comments
+
   belongs_to :user
+
   has_many :taggings
+
+  has_many :tags,
+    through: :taggings,
+    source: :tag
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,7 @@
 class Tag < ActiveRecord::Base
   has_many :taggings
-  
+
+  has_many :photos,
+    through: :taggings,
+    source: :photo
 end
