ff26e186c580e07611ee3937aa20fcd1e9da0275
Maximilian Rutyna <mrutyna777@gmail.com>
2016-07-05 14:26:06 -0700

    Cloudinary Works

--- app/views/api/photos/show.json.jbuilder
+++ app/views/api/photos/show.json.jbuilder
@@ -1,6 +1,6 @@
 if @photo
   json.extract!(@photo, :title, :description, :photo_url, :id)
-  json.username @photo.user.username
+  # json.username @photo.user.username
   json.comments @photo.comments, partial: 'api/comments/comment', as: :comment
 end
 
--- frontend/components/navbar/navbar_right.jsx
+++ frontend/components/navbar/navbar_right.jsx
@@ -1,12 +1,28 @@
 const React = require('react');
 const Link = require('react-router').Link;
 const SessionActions = require('../../actions/session_actions');
+const PhotoActions = require('../../actions/photo_actions.js');
 
 const SessionStore = require('../../stores/session_store');
 
 const NavBarRight = React.createClass({
 
-
+  uploadImage() {
+    cloudinary.openUploadWidget({ cloud_name: 'mrcapstone', upload_preset: 'gvnvjyr9'},
+            function(error, result) {
+              if (error) { console.log(error); } else {
+              let photo_url = result[0].secure_url;
+              let title = prompt('What is the title of this picture ?');
+              let description = prompt('What is the description of this picture ?');
+              let photo = {title: title,
+                          description: description,
+                          photo_url: photo_url,
+                          user_id: SessionStore.currentUser().id};
+              console.log(photo);
+              PhotoActions.createPhoto(photo);
+            };
+            });
+  },
 
   render () {
 
@@ -16,6 +32,7 @@ const NavBarRight = React.createClass({
     		<hgroup className="header-group">
     			<h2 className="header-name">Hi, {SessionStore.currentUser().username}!</h2>
     			<input className="header-button" type="submit" value="logout" onClick={ SessionActions.logOut } />
+          <button onClick={this.uploadImage}>Upload Image</button>
     		</hgroup>
     	);
 
